b2a572d2603d0d09fd695e45da77c7986bc7afe9
GazuaDev8417 <francadasilvaflamarion@gmail.com>
2026-06-25 15:31:17 -0300

    retirada das declarações de useref e usestate, pois não estão sendo utilizadas

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from 'react'
+import { useContext, useEffect } from 'react'
 import Context, { GlobalStateContext } from '../../global/Context'
 import { useNavigate } from 'react-router-dom'
 import { MdEdit } from 'react-icons/md'
